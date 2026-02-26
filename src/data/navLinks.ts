183995915c5ff0c9beda5489b72e50ea3a6c969e
aditya chauhan <imadityachauhan149@gmail.com>
2026-02-26 20:47:31 +0530

    addded project section

--- src/data/navLinks.ts
+++ src/data/navLinks.ts
@@ -14,7 +14,7 @@ export const navLinks: NavLink[] = [
   { name: 'HOME', href: '/', hasDropdown: false }, 
   { name: 'ABOUT US', href: '/about', hasDropdown: false },
   { name: 'SERVICES', href: '/services', hasDropdown: false }, 
-  { name: 'PROJECTS', href: '/#projects', hasDropdown: true },
+  { name: 'PROJECTS', href: '/projects', hasDropdown: false }, // <-- Updated
   { name: 'OUR CLIENTS', href: '/#clients', hasDropdown: false },
   { name: 'PRODUCTS', href: '#', hasDropdown: true },
   { name: 'CONTACT', href: '/#contact', hasDropdown: false },
@@ -27,6 +27,5 @@ export const topBarLinks: Omit<NavLink, 'hasDropdown'>[] = [
 ];
 
 export const socialLinks: SocialLink[] = [
-  // Kept only LinkedIn
   { name: 'LinkedIn', icon: 'FaLinkedinIn', href: 'https://www.linkedin.com/company/110518013/' },
 ];
